ddb49deb0f9f31fbd811fb67a85ac75ac8429981
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2023-02-10 17:53:24 +0000

    feat: Add animation to the skills component

--- web/src/components/Skills/Skills.styled.js
+++ web/src/components/Skills/Skills.styled.js
@@ -4,7 +4,7 @@ export const StyledSkills = styled.div`
 
     width: 100%;
     height: auto;
-    margin: 140px 10px;
+    margin: 140px 10px 80px;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -12,7 +12,7 @@ export const StyledSkills = styled.div`
 
     h2 {
         align-self: flex-start;
-        margin-bottom: 50px;
+        margin: 50px 0 50px 5px;
     }
 
     .skills-brief {
@@ -22,7 +22,7 @@ export const StyledSkills = styled.div`
     .skills-container {
         width: 80%;
         height: auto;
-        margin: 30px 0 80px;
+        margin: 30px 0;
         //background-color: ${({ theme }) => theme.colors.secondaryTransparent};
         border-radius: 15px;
         padding: 15px;
@@ -113,22 +113,32 @@ export const StyledSkills = styled.div`
         display: none;
     }
 
-    .skill-card {
-        width: 100px;
-        height: 100px;
-        background-color: ${({ theme }) => theme.colors.primaryMediumShade};
-        box-shadow: inset 0 0 25px ${({ theme }) => theme.colors.secondaryLightTransparent};
-        border-radius: 25px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin: 20px 10px;
-        padding-top: 15px;
-        position: relative;
+    
+`
+
+export const StyledSkillCard = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: ${({ theme }) => theme.colors.primaryMediumShade};
+    box-shadow: inset 0 0 25px ${({ theme }) => theme.colors.secondaryLightTransparent};
+    border-radius: 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 10px;
+    padding-top: 15px;
+    position: relative;
+    animation: up 1s forwards alternate;
+    animation-duration: calc(10s / ${({ speed }) => speed });
+    
+
+    @keyframes up {
+        0% {transform: scale(0);}
+        100%{transform: scale(1);}
     }
 
-    .skill-card:hover {
+    :hover {
         animation: flip 1s ease-in-out;
     }
 
@@ -148,12 +158,12 @@ export const StyledSkills = styled.div`
     .skill-icon {
         width: 35px;
         height: 35px;
-        color: ${({ theme }) => theme.colors.secondaryLightTransparent};
+        color: ${({ theme }) => theme.colors.secondaryLightNeon};
     }
 
-    .skill-card p {
+    p {
         font-size: 0.6em;
-        color: ${({ theme }) => theme.colors.secondaryLightShade};
+        color: ${({ theme }) => theme.colors.secondaryLightNeon};
         margin-top: 7px;
     }
 `
--- web/src/components/Skills/index.jsx
+++ web/src/components/Skills/index.jsx
@@ -1,17 +1,17 @@
-import { StyledSkills } from './Skills.styled'
+import { StyledSkills, StyledSkillCard } from './Skills.styled'
 import { TiHtml5 } from 'react-icons/ti'
 import { IoLogoCss3 } from 'react-icons/io'
-import { SiJavascript, SiBabel } from 'react-icons/si'
+import { SiJavascript, SiBabel, SiStyledcomponents } from 'react-icons/si'
 import { FaReact, FaNode, FaGitAlt, FaSass, FaBookReader, FaPeopleCarry } from 'react-icons/fa'
 import { GiLoveMystery } from 'react-icons/gi'
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 export function Skills() {
 
     const [isHardSkills, setIsHardSkills] = useState(true);
 
     const toggleSkills = ()=> {
-
+        setIsHardSkills(!isHardSkills);
     }
 
     return(
@@ -20,70 +20,75 @@ export function Skills() {
             <p className='skills-brief'>Through a combination of academic study and hands-on experience in personal projects, I have developed technical skills and knowledge. I am a self-motivated learner who is always eager to expand my skillset and stay up-to-date with the latest developments in the field.</p>
 
             <div className='button-switch' >
-                <button onClick={()=>{setIsHardSkills(!isHardSkills)}} className={isHardSkills? 'hard-skills-button button-on' : 'hard-skills-button button-off'} >HARD SKILLS</button>
-                <button onClick={()=>{setIsHardSkills(!isHardSkills)}} className={isHardSkills? 'soft-skills-button button-off' : 'soft-skills-button button-on'} >SOFT SKILLS</button>
+                <button onClick={toggleSkills} className={isHardSkills? 'hard-skills-button button-on' : 'hard-skills-button button-off'} >HARD SKILLS</button>
+                <button onClick={toggleSkills} className={isHardSkills? 'soft-skills-button button-off' : 'soft-skills-button button-on'} >SOFT SKILLS</button>
             </div>
             
 
             <div className='skills-container'>
 
                 <div className={isHardSkills? 'hard-skills displayFlex' : 'hard-skills displayNone'} >
-                    <div className='skill-card'  >
+                    <StyledSkillCard speed={11} >
                         <TiHtml5 className='skill-icon' />
-                        <p className=''> HTML</p>
-                    </div>
+                        <p> HTML</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={6} >
                         <IoLogoCss3 className='skill-icon' />
-                        <p className=''> CSS</p>
-                    </div>
+                        <p> CSS</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={9} >
                         <SiJavascript className='skill-icon' />
-                        <p className=''> Javascript</p>
-                    </div>
+                        <p> Javascript</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={25} >
                         <FaReact className='skill-icon' />
-                        <p className=''> ReactJs</p>
-                    </div>
+                        <p> ReactJs</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={14} >
                         <FaNode className='skill-icon' />
-                        <p className=''> NodeJs</p>
-                    </div>
+                        <p> NodeJs</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={21} >
                         <FaGitAlt className='skill-icon' />
-                        <p className=''> Git/Github</p>
-                    </div>
+                        <p> Git/Github</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={11} >
                         <FaSass className='skill-icon' />
-                        <p className=''> Sass</p>
-                    </div>
+                        <p> Sass</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={6} >
+                        <SiStyledcomponents className='skill-icon' />
+                        <p> Styled Component</p>
+                    </StyledSkillCard>
+
+                    <StyledSkillCard speed={9} >
                         <SiBabel className='skill-icon' />
-                        <p className=''> Babel</p>
-                    </div>
+                        <p> Babel</p>
+                    </StyledSkillCard>
                 </div>
                
                <div className={isHardSkills? 'soft-skills displayNone' : 'soft-skills displayFlex'}>
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={11} >
                         <GiLoveMystery className='skill-icon' />
-                        <p className=''> Passion for coding</p>
-                    </div>
+                        <p> Passion for coding</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={6} >
                         <FaBookReader className='skill-icon' />
-                        <p className=''> Eager to learn</p>
-                    </div>
+                        <p> Eager to learn</p>
+                    </StyledSkillCard>
 
-                    <div className='skill-card' >
+                    <StyledSkillCard speed={9} >
                         <FaPeopleCarry className='skill-icon' />
-                        <p className=''> team player</p>
-                    </div>
+                        <p> team player</p>
+                    </StyledSkillCard>
                </div>
                 
             </div>
