745312094f0f5336441b3a12f39736dcf7059f3b
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2023-02-19 18:02:59 +0000

    feat: Add the Footer component to the page

---
+++ web/src/components/Footer/Footer.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    small {
+        text-align: center;
+        //color: ${({ theme }) => theme.colors.tertiaryMediumShade};
+    }
+`
---
+++ web/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import { StyledFooter } from './Footer.styled';
+import { AiFillHeart } from 'react-icons/ai'
+
+export function Footer() {
+     
+    return(
+        <StyledFooter>
+            <small>Designed and Developed by me <AiFillHeart /> Janaina Marostega de Jesus ©2023</small>
+        </StyledFooter>
+    )
+
+}
