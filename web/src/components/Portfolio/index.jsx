eed41bf5423a6d43bf2baf029ebb9644b5467b93
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2023-02-21 18:01:00 +0000

    fix: Fix a typing mistake

--- web/src/components/Portfolio/index.jsx
+++ web/src/components/Portfolio/index.jsx
@@ -23,9 +23,9 @@ export function Projects() {
                     <h4>Got a Plan - Travel Planner Web App</h4>
                     <div className='featured-project-img'></div>
                     <p>Are you planning a trip for the next year but unsure if you'll be able to do all the activities you want because of unpredictable weather? With Got a Plan, you can get the most accurate weather forecast for your upcoming trip, <strong>whether you're leaving in a week or months ahead</strong>. If the trip is within a week, you will receive the current weather forecast. If the trip is further out, you will receive a predicted forecast, so you can choose the best date for your trip, event or day out. </p>
-                    <p>Got a Plan is a SPA (Single Page Application) developed by, me using several tools and packages. It also has a Jest testing unit and the functionalities were built modularly allowing for better maintenance.</p>
-                    <strong>Deploy:</strong> <a href="https://gotaplan.onrender.com" target='blank' > check it out! </a> <br />
-                    <strong>Github repository:</strong> <a href="https://github.com/Janaina-MJ/FEND-Capstone-travel-app.git" target='blank' > check the code here! </a> <br/ >
+                    <p>Got a Plan is a SPA (Single Page Application) developed by me using several tools and packages. It also has a Jest testing unit and the functionalities were built modularly allowing for better maintenance.</p>
+                    <strong>Deploy:</strong> <a href="https://gotaplan.onrender.com" target='_blank' > check it out! </a> <br />
+                    <strong>Github repository:</strong> <a href="https://github.com/Janaina-MJ/FEND-Capstone-travel-app.git" target='_blank' > check the code here! </a> <br/ >
                     <ul className='tech-stack'>
                         <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target='blank' >HTML</a></li>
                         <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target='blank'>JavaScript</a></li>
